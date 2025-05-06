987c528c9e299881643ea5c9ece2c3b64a034e53
Lasse Egebo <lae003@edu.zealand.dk>
2025-05-06 11:16:00 +0200

    translate print statement in client.py to English

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1) #Enable broadcast
 
 # Serielforbindelse til GPS
 with serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=1) as ser:
-    print("Læser rå GPS-data og sender via UDP...")
+    print("Reads raw GPS data and sends over UDP...")
     while True:
         line = ser.readline().decode('ascii', errors='ignore').strip()
         if line.startswith('$'):  # Tjek om det er en NMEA-sætning
