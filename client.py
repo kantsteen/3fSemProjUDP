554c629d19b258db31ca3ea10f26943e98fedff4
Lasse Egebo <lae003@edu.zealand.dk>
2025-05-06 11:20:45 +0200

    refactor: rename variables for clarity and improve print statement consistency in client.py

--- client.py
+++ client.py
@@ -1,16 +1,16 @@
 from socket import * 
 from serial import *
 
-serverName= '255.255.255.255' #Change to Azure server
-serverPort=12000
+UDP_IP = '255.255.255.255' #Change to Azure server
+UDP_PORT = 12000
 sock = socket(AF_INET, SOCK_DGRAM)
 sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1) #Enable broadcast
 
 # Serielforbindelse til GPS
-with Serial("/dev/ttyAMA0", baudrate=9600, timeout=1) as ser:
-    print("Reads raw GPS data and sends over UDP...")
+with Serial("/dev/ttyAMA0", baudrate = 9600, timeout = 1) as ser:
+    print("Reads raw GPS data and sends via UDP...")
     while True:
-        line = ser.readline().decode('ascii', errors='ignore').strip()
+        line = ser.readline().decode('ascii', errors = 'ignore').strip()
         if line.startswith('$'):  # Tjek om det er en NMEA-sætning
             print("Sender:", line)
-            sock.sendto(line.encode(), (UDP_IP, UDP_PORT))
+            sock.sendto(line.encode(), (UDP_IP, UDP_PORT)) 
