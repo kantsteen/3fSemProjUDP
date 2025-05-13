2af37d78c130846a040d3e6858390dcead892be0
Mikkel Thomsen <mikk52j0@edu.zealand.dk>
2025-05-13 13:32:25 +0200

    feat: add start_button.py to manage COASTUDP process with GPIO button

---
+++ start_button.py
@@ -0,0 +1,24 @@
+from gpiozero import Button
+from subprocess import Popen
+from signal import pause
+import os
+
+button = Button(17)
+gps_process = None  # Track the GPS script process
+
+def toggle_script():
+    global gps_process
+    if gps_process is None:
+        print("Button pressed. Starting COASTUDP...")
+        gps_process = Popen(["python3", "/home/pi/COAST/COASTUDP.py"])
+    else:
+        print("Button pressed again. Stopping COASTUDP...")
+        gps_process.terminate()
+        gps_process.wait()
+        gps_process = None
+        print("COASTUDP stopped and cleaned up.")
+
+print("Ready. Press button to start/stop COASTUDP.")
+button.when_pressed = toggle_script
+
+pause()  # Keep script running
